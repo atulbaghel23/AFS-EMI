e012347baa1c986850384d5d2ebdb49389b85382
atul231001 <atulbaghele2001@gmail.com>
2026-06-22 17:15:29 +0530

    login responce

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -70,19 +70,43 @@ export const login = async (req, res) => {
 
     if (user && (await bcrypt.compare(password, user.password))) {
       res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-        roleId: user.roleId,
-        customerId: user.customerId,
-        supervisorId: user.supervisorId || null,
-        type: user.type || user.customerId?.type,
-        status: user.status,
-        settings: user.settings,
-        mustResetPassword: user.mustResetPassword || false,
-        token: generateToken(user._id)
+        // _id: user._id,
+        // name: user.name,
+        // email: user.email,
+        // role: user.role,
+        // roleId: user.roleId,
+        // customerId: user.customerId,
+        // supervisorId: user.supervisorId || null,
+        // type: user.type || user.customerId?.type,
+        // status: user.status,
+        // settings: user.settings,
+        // mustResetPassword: user.mustResetPassword || false,
+        // token: generateToken(user._id)
+
+        "success": true,
+        "statusCode": 200,
+        "message": "Data retrieved successfully",
+        "data": {
+          _id: user._id,
+          name: user.name,
+          role: user.role,
+          email: user.email
+        },
+
+        // roleId: user.roleId,
+        // customerId: user.customerId,
+        // supervisorId: user.supervisorId || null,
+        // type: user.type || user.customerId?.type,
+        // status: user.status,
+        // settings: user.settings,
+        // mustResetPassword: user.mustResetPassword || false,
+        token: generateToken(user._id),
+        "error": null
+
       });
+
+
+
     } else {
       res.status(401).json({ message: 'Invalid email, password or role' });
     }
